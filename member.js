b03bd3e67c248a320a06f42d398e2ef035767cf9
Gabriel Orphiano <gab.orphiano@gmail.com>
2023-07-22 14:10:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillMembers() {
+  return {
+    restrict: 'E',
+    templateUrl: 'skill-members.html',
+    controller: 'SkillMembersController',
+    controllerAs: 'skillMembersCtrl',
+    bindToController: true,
+    scope: {
+      skill: '='
+    }
+  };
+}
